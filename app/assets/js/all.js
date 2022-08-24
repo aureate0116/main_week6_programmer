27963f10fae8a8e707667663056452dae43106a9
aureate <aureate16@gmail.com>
2022-08-24 22:09:24 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,15 +3,34 @@ $(function() {
 
  
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 8,
-    spaceBetween: 16,
+    //slidesPerView: 8,
+    spaceBetween: 8,
     loop:true,
    
     autoplay: {
       speed:3000,
       disableOnInteraction: true
-    }
+    },
     
+    breakpoints: {  
+      '360': {
+        slidesPerView: 3,
+        },
+      '480': {
+        slidesPerView: 3,
+        },
+      '720': {
+        slidesPerView: 4,
+        },
+      '960': {
+        slidesPerView: 6,
+        },
+      '1440': {
+        slidesPerView: 8,
+        },
+    },
+    
+
     // pagination: {
     //   el: ".swiper-pagination",
     //   clickable: true,
@@ -20,18 +39,10 @@ $(function() {
 
 
   // 學生分享輪播
-
-  var swiper = new Swiper(".swiper", {
+  var swiper = new Swiper(".swiper2", {
     effect: "fade",
-    // fadeEffect: {
-    //   crossFade: true
-    // },
-
-    //grabCursor: true,
-    
     pagination: {
       el: ".swiper-pagination",
-     // clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
