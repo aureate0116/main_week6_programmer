90f477212675b0f1abd2a4c175bc4654845c3529
aureate <aureate16@gmail.com>
2022-08-24 16:07:26 +0800

    Update 2022-08-24T08:07:21.831Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,35 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  //console.log('Hello Bootstrap5');
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    spaceBetween: 16,
+    loop: true,
+    autoplay: {
+      speed: 3000,
+      disableOnInteraction: true
+    } // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+
+  }); // 學生分享輪播
+
+  var swiper = new Swiper(".swiper", {
+    effect: "fade",
+    // fadeEffect: {
+    //   crossFade: true
+    // },
+    //grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination" // clickable: true,
+
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
